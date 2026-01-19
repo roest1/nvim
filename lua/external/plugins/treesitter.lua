288d5c50ba21fe989aa69939720ae3842107553c
roest1 <95721769+roest1@users.noreply.github.com>
2026-01-19 13:12:20 -0600

    upgrade treesitter

--- lua/external/plugins/treesitter.lua
+++ lua/external/plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {
   'nvim-treesitter/nvim-treesitter',
 
-  tag = 'v0.9.2',
+  tag = 'v0.10.0',
+  lazy = false,
+  build = ':TSUpdate',
   event = { 'BufReadPost', 'BufNewFile' }, -- run on writes
 
   config = function()
